e65e78d1e9a7f5f7df400080ff0cbf90a18616ba
cheeseonamonkey <cheeseonamonkey@gmail.com>
2023-10-16 02:12:12 -0600

    access control allow origin header

--- index.js
+++ index.js
@@ -9,24 +9,26 @@ const sqlite3 = require('sqlite3');
 
 app.use(express.json());
 app.use(cors());
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    // You can add more headers as needed (e.g., Access-Control-Allow-Methods, Access-Control-Allow-Headers).
-    next();
+  // this will set it dynamically to the current URL's origin: 
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  next();
 });
+
 
 app.post('/upload', upload.single('sqlFile'), async (req, res) => {
   try {
     if (!req.file) {
       throw new Error('Request does not contain a file.');
     }
-
     const dbFilePath = req.file.path;
 
     // Process the SQL file and generate the Mermaid diagram
     const md = await generateMermaidDiagram(dbFilePath);
     console.log(md + ' generated');
 
+    // returns the diagram as a ...?
     res.status(200).send(md);
   } catch (error) {
     console.error(error);
@@ -123,8 +125,8 @@ const generateMermaidDiagram = async (dbFilePath) => {
 
       columns.forEach((column) => {
         const { name, type } = column;
-         let dataType = type.replace(/,/g, '-') || "UNDEFINED";
-       
+        let dataType = type.replace(/,/g, '-') || "UNDEFINED";
+
         const isPrimaryKey = primaryKeys.includes(name);
         const isForeignKey = foreignKeyColumns.find((fk) => fk.from === name);
         mermaidDiagram += `        ${name} ${dataType}`;
@@ -136,7 +138,7 @@ const generateMermaidDiagram = async (dbFilePath) => {
         }
         mermaidDiagram += '\n';
         dataType = null;
-      });      
+      });
     }
 
     mermaidDiagram += '    }\n';
@@ -152,8 +154,7 @@ const generateMermaidDiagram = async (dbFilePath) => {
   db.close(() => {
     fs.writeFileSync('./out/er_diagram.mermaid', mermaidDiagram);
     //delete temporary db file
-    fs.unlink(dbFilePath, (err) =>
-    {if (err) throw err;});
+    fs.unlink(dbFilePath, (err) => { if (err) throw err; });
   });
   return mermaidDiagram;
 }
